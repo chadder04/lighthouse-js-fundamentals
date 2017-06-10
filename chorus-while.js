e9aca77cfb6e54035770a1a72729e66ad1392d35
Chad Garrett <chadgarrett_@hotmail.com>
2017-06-10 21:49:33 +0000

    Added looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,8 @@
+// Looping Exercises
+// Chad Garrett June 10 2017
+
+var chorus = "Let's dance!";
+for (var repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
---
+++ packing-list.js
@@ -0,0 +1,9 @@
+// Iterating over an array
+// Chad Garrett June 10 2017
+
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (var i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
